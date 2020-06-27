fcc2fc91f86640c367bba3a1559648e0e865d011
Shamil Frontend <sh.alisultanov@maximatelecom.ru>
2020-06-27 20:01:12 +0300

    fix

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
 const tasksListElement = document.querySelector(`.tasks__list`);
-const taskElements = tasksListElement.querySelectorAll(`.tasks__item`);
 
-for (const task of taskElements) {
-    task.draggable = true;
-}
-
-tasksListElement.addEventListener(`dragstart`, (evt) => {
-    evt.target.classList.add(`selected`);
+tasksListElement.addEventListener(`dragstart`, (e) => {
+    e.target.classList.add(`selected`);
 });
 
-tasksListElement.addEventListener(`dragend`, (evt) => {
-    evt.target.classList.remove(`selected`);
+tasksListElement.addEventListener(`dragend`, (e) => {
+    e.target.classList.remove(`selected`);
 });
 
 const getNextElement = (cursorPosition, currentElement) => {
-    const currentElementCoord = currentElement.getBoundingClientRect();
-    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+    const currentElementCoords = currentElement.getBoundingClientRect();
+    const currentElementCenter = currentElementCoords.y + currentElementCoords.height / 2;
 
     return (cursorPosition < currentElementCenter) ?
         currentElement :
         currentElement.nextElementSibling;
 };
 
-tasksListElement.addEventListener(`dragover`, (evt) => {
-    evt.preventDefault();
+tasksListElement.addEventListener(`dragover`, (e) => {
+    e.preventDefault();
 
     const activeElement = tasksListElement.querySelector(`.selected`);
-    const currentElement = evt.target;
+    const currentElement = e.target;
     const isMoveable = activeElement !== currentElement && currentElement.classList.contains(`tasks__item`);
 
     if (!isMoveable) return;
 
-    const nextElement = getNextElement(evt.clientY, currentElement);
+    const nextElement = getNextElement(e.clientY, currentElement);
 
     if (
         nextElement &&
